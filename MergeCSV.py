4422c3376fe10c919552f0ba5f29d147529bf7a8
niklaas-0rbit <niklaas@0rb.it>
2020-12-16 16:12:34 +0100

    update

--- MergeCSV.py
+++ MergeCSV.py
@@ -12,35 +12,19 @@ import os
 import sys
 
 
-
-# choices for argument-parsing
-# flowsampling-modes
-flowsmode = {1:'every n-th packet',2:'sample & skip n packets',3:'sample first n packets of a flow',4:'sample n, skip n-1, sample n-2 ...'}
-# packetsampling-modes
-packetsmode = {1:'every n-th packet'}
-# capture files, https://www.unb.ca/cic/datasets/ids-2017.html
-filenames = {0:'All',1:'Monday-WorkingHours',2:'Tuesday-WorkingHours',3:'Wednesday-WorkingHours',4:'Thursday-WorkingHours',5:'Friday-WorkingHours'}
-# feature vectors
-featurevectors = {1:'AGM_10s.json', 2:'AGM_60s.json',3:'AGM_3600s.json',4:'CAIA_flowSampling.json',5:'CAIA_packetSampling.json'}
-
 # directories
 #rpi
-#flowfolder = '/mnt/data/CIC-IDS2017/PCAP/flow-sampledCSV'
-#packetfolder = '/mnt/data/CIC-IDS2017/PCAP/packet-sampledCSV'
 flowfolder = '/home/dietpi/BSc-e1027075/rpi/flow-sampled'
 packetfolder = '/home/dietpi/BSc-e1027075/rpi/packet-sampled'
-
 
 
 # ARGUMENT PARSING
 import argparse
 parser = argparse.ArgumentParser(description='Script to merge multiple CSVs into a single file.')
-
-# force sampling method & mode
+# force choice for folder-selection
 samplegroup = parser.add_mutually_exclusive_group(required=True)
 samplegroup.add_argument('--flowsampling', action='store_true', help='merge flow-sampled CSVs')
 samplegroup.add_argument('--packetsampling',action='store_true', help='merge packet-sampled CSVs')
-
 args = parser.parse_args()
 
 
