1c8aed9a6156f1253a3741757413303416d00f97
niklaas-0rbit <niklaas@0rb.it>
2021-01-21 15:29:45 +0100

    Update Evaluation.py

--- Evaluation.py
+++ Evaluation.py
@@ -9,6 +9,9 @@ Created on Fri Jan 21 13:24:18 2021
 from pandas import read_csv
 from pathlib import Path, PureWindowsPath
 
+import pandas as pd
+import numpy as np
+
 
 # import CSV
 def importCSV(csvpath,csvusecols=None,verbose=False,chunksize=None,encoding='utf-8'):  
@@ -53,17 +56,47 @@ resultcsv = logd / 'result.csv'
 timecsv = logd / 'time.csv'
 dstatcsv = logd / 'dstat.csv'
 
-
 if __name__ == '__main__':
 
+    pd.set_option('display.float_format', lambda x: '%.5f' % x) # force float output for epoch time
 
-	# IMPORT
+    # IMPORT
+    timestamps = read_csv(timecsv,delimiter=',',encoding='utf-8')
+    print(timestamps)
+    
+    dstat = read_csv(dstatcsv,delimiter=r'[,\t]',header=5,encoding='utf-8')
+    print(dstat)
 
-	# timestamps
-	timestamps = read_csv(timecsv,delimiter=',',encoding='utf-8')
-	print(timestamps)
+    report = read_csv(reportcsv,delimiter=',',encoding='utf-8')
+    print(report)
 
-	dstat = read_csv(dstatcsv,delimiter=r'[,\t]',header=5,encoding='utf-8')
-	print(dstat)
+    result = read_csv(resultcsv,delimiter=',',encoding='utf-8')
+    print(result)
 
-	exit()
+    accuracyscore = float(result['summary'][2])
+    print('\n{}'.format(accuracyscore))
+    print(type(accuracyscore))
+
+    matrix = result['summary'][4]
+    print('\n\n{}\n'.format(matrix))
+
+    # get actual numbers from saved confusion-matrix
+    matrix = matrix.replace('\n',r'').replace('[',r'').replace(']',r'') # remove unnecessary characters
+
+    s=''
+    npmatrix = np.empty(shape=[0,4])
+    index = 0
+    for string in matrix.split():
+    	index += 1
+    	for character in string:
+    		if character.isdecimal():
+    			s += character
+    	#print('{}: {} {}'.format(index,type(s),s))
+    	if s.isdigit(): 
+    		i = int(s)
+    		npmatrix = np.append(matrix,i,axis=1)
+    		#print('{}: {} {}'.format(index,type(i),i))
+    	s = ''
+
+    print(npmatrix)
+    exit()
