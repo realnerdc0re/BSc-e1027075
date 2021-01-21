f08bcd891f65788778abb84240b999df573c3e83
niklaas-0rbit <niklaas@0rb.it>
2021-01-21 14:27:02 +0100

    Update Evaluation.py

--- Evaluation.py
+++ Evaluation.py
@@ -6,3 +6,64 @@ Created on Fri Jan 21 13:24:18 2021
 @author: pjr
 """
 
+from pandas import read_csv
+from pathlib import Path, PureWindowsPath
+
+
+# import CSV
+def importCSV(csvpath,csvusecols=None,verbose=False,chunksize=None,encoding='utf-8'):  
+
+    if time: start = timer()
+
+    # informational output
+    print('\n\n'+40*'~'+' FUNCTION: importCSV (chunksize: {}) '.format(chunksize)+40*'~')
+    print('\n>>> importing CSV: {}'.format(csvpath))
+
+    csvdata = pd.DataFrame() # initialise empty dataframe
+
+    # if no chunksize is given, read CSV in one step, otherwise read in chunks
+    if chunksize == None:
+        csvdata = read_csv(csvpath,usecols=csvusecols,skipinitialspace=True,encoding=encoding)
+    # chunksize determines numbers of rows per chunk
+    else:
+        for chunk in read_csv(csvpath,usecols=csvusecols,skipinitialspace=True,encoding=encoding,chunksize=chunksize):
+            csvdata = csvdata.append(chunk)
+
+    printdata(csvdata,'imported')
+
+    if verbose:
+        print('\n{}'.format(csvdata.groupby('Label').size()))
+        print('\n{}'.format(csvdata.groupby('Attack').size()))
+        if (not time): input('\n...')
+
+    if time: 
+        end = timer()
+        print('\nimportCSV\n[TIME]: %.3f' % (end-start),'seconds')
+
+    return csvdata
+
+
+# working directory
+wd = Path.cwd()
+# logs
+logd = wd / 'logs'
+#logd = wd / 'rpi-logs'
+reportcsv = logd / 'report.csv'
+resultcsv = logd / 'result.csv'
+timecsv = logd / 'time.csv'
+dstatcsv = logd / 'dstat.csv'
+
+
+if __name__ == '__main__':
+
+
+	# IMPORT
+
+	# timestamps
+	timestamps = read_csv(timecsv,delimiter=',',encoding='utf-8')
+	print(timestamps)
+
+	dstat = read_csv(dstatcsv,delimiter=r'[,\t]',header=5,encoding='utf-8')
+	print(dstat)
+
+	exit()
