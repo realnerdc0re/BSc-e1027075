533c80a09c1d0a19a08d3400ea3243ed365a5e82
niklaas-0rbit <niklaas@0rb.it>
2021-02-18 15:35:06 +0100

    Update Master.py
    implemented description, automated script executions based on given configuration

--- Master.py
+++ Master.py
@@ -6,54 +6,98 @@ Created on Sat Jan 17 13:53:04 2021
 @author: pjr
 """
 
-import csv
 import os
-import sys
-import pandas as pd
-import time as epochtime
-
-from pathlib import Path, PureWindowsPath, PurePath, PurePosixPath
-
-
-
-# import experiment-configurations
 import config as cfg
+from colorama import Fore, Style
+from timeit import default_timer as timer
 
-# MISSING: create all commands from configuration or file within iterations to work through experiments
+
+
+
 # MISSING: mv original PCAP files into other folder, use editcap to just get small splitfile for testing purpuse speeeding up tests
+
+
+# ARGUMENT PARSING
+import argparse
+parser = argparse.ArgumentParser(description='Script to automate experiments based on a given configuration via config.py. The Script does the sampling and model-creation on the local machine and syncs all necessary files from the local machine to a remote machine afterwards. Subsequently it creates predictions on the remote machine, saves the results and syncs back to the local machine.')
+parser.add_argument('-v','--verbose', action='store_true', help='output verbose information')
+args = parser.parse_args()
 
 
 if __name__ == '__main__':
 
+    start = timer()
 
-	# informational output
-	print('file: {}'.format(cfg.file))
-	print('modes: {}'.format(cfg.mode))
-	print('steps: {}'.format(cfg.steps))
-	print('vectors: {}'.format(cfg.vector))
-	input('blub')
+    verbose = args.verbose
 
-	samplingcmd = 'python3 -u rpi-Sampling.py -e -f 1 5 5 4'
-	modelcreationcmd = '' # doing rpi-Preprocessing on the local machine, saving the fitted RandomForest model
+    # check passed optional arguments and commands
+    print('\n'+40*'~'+' SCRIPT: Master.py '+40*'~')
+    if verbose:
+        print('\nfiles:\n\t{}\n\nsampling-modes:\n\t{}\n\nfeature-vectors:\n\t{}\n'.format(cfg.filenames,cfg.modes,cfg.vectors))
+        print(20*'~'+' configuration '+20*'~')
+        print('\n\tfile:\t{}\n\tvector:\t{}\n\tmode:\t{}\n\tstep:\t{}\n\n'.format(cfg.file,cfg.vector,cfg.mode,cfg.steps))
+        input('...')
 
-	# COMMANDS
-	# creating base-directory
-	mkdircmd = "ssh dietpi@10.10.45.55 'mkdir -p /mnt/data/CIC-IDS2017/PCAP/flow-sampledCSV/'"
-	# forging command to sync sampled files and pre-processed model to VM/rpi (should be done via configuration file or lists defined in this script, containing sampling methods, steps...)
-	synccmd = r'rsync -avz --progress /mnt/data/CIC-IDS2017/PCAP/flow-sampledCSV/Friday-WorkingHours_mode1_vector4_steps5_perflowsampled/ dietpi@10.10.45.55:/mnt/data/CIC-IDS2017/PCAP/flow-sampledCSV/Friday-WorkingHours_mode1_vector4_steps5_perflowsampled/'
-	# cmd to execute Preprocessing & Classification on VM/rpi from local machine
-	sshcmd = "ssh dietpi@10.10.45.55 'cd BSc-e1027075 && python3 -u rpi-Preprocessing.py -e -r -f 1 5 5 4 100000'"
+    # check online-status of remote machine
+    ping = os.system('ping -c 1 {} >/dev/null 2>&1'.format(cfg.remoteip)) # ping remote machine
+    if ping == 0: print('>>> Status {}: '.format(cfg.remoteip)+Fore.GREEN+'ONLINE'+Style.RESET_ALL)
+    else: print('>>> Status {}: '.format(cfg.remoteip)+Fore.RED+'OFFLINE'+Style.RESET_ALL), exit()
 
+    # iterate over configurations
+    for file in cfg.file:
+        for vector in cfg.vector: # iterate over given feature-vectors
 
+            # folder selection based on given feature-vector
+            if vector < cfg.vectorlimit:
+                basefolder = cfg.flowfolder
+                sarg = '-f'
+                foldername = '{}_mode{}_vector{}_steps{}_perflowsampled'
+            else:
+                basefolder = cfg.packetfolder
+                sarg = '-p'
+                foldername = '{}_mode{}_vector{}_steps{}_packetsampled'
+            mkdircmd = "ssh {} 'mkdir -p {}'".format(cfg.remote,basefolder)
 
-	# EXECUTION (packed in loops, working through configuration lists given in config.py)
+            for mode in cfg.mode: # iterate over given sampling-modes
 
-	#os.system(samplingcmd)
-	#os.system(modelcreationcmd)
-	os.system(mkdircmd)
-	os.system(synccmd)
-	os.system(sshcmd)
-	#missing syncing results from VM/rpi back to local machine
+                # check plausibility of given configuration, continue on pointless combinations
+                if vector < cfg.vectorlimit and mode >= cfg.samplinglimit: continue # perflow feature-vectors and packetsampling-modes
+                elif vector >= cfg.vectorlimit and mode < cfg.samplinglimit: continue #packetsampling feature-vectors and perflowsampling-modes
 
+                for steps in cfg.steps: # iterate over given sampling-steps
 
-	exit()
+                    # forge folders & commands based on configuration
+                    currentfolder = foldername.format(cfg.filenames[file],mode,vector,steps)
+                    samplingcmd = 'python3 rpi-Sampling.py -e {} {} {} {} {}'.format(sarg,mode,file,steps,vector)
+                    modelcmd = 'python3 rpi-Preprocessing.py -e -s {} {} {} {} {} {}'.format(sarg,mode,file,steps,vector,cfg.batchsize)
+                    synccmd = r'rsync -avz --progress {}/{}/ {}:{}/{}/'.format(basefolder,currentfolder,cfg.remote,basefolder,currentfolder)
+                    sshcmd = "ssh {} 'cd {} && python3 -u rpi-Preprocessing.py -e -r -m {} {} {} {} {} {}'".format(cfg.remote,cfg.remotewd,sarg,mode,file,steps,vector,cfg.batchsize) # importing model on remote
+                    #sshcmd = "ssh {} 'cd {} && python3 -u rpi-Preprocessing.py -e -r -s {} {} {} {} {} {}'".format(cfg.remote,cfg.remotewd,sarg,mode,file,steps,vector,cfg.batchsize) # saving model on remote
+                    resynccmd = r'rsync -avz --progress {}:{}/{}/ {}/{}/'.format(cfg.remote,basefolder,currentfolder,basefolder,currentfolder)
+
+                    # subsequential script & command exections
+                    print('>>> Sample PCAP on local machine: {}'.format(samplingcmd))
+                    os.system(samplingcmd)
+
+                    print('>>> Create and save model on local machine: {}'.format(modelcmd))
+                    os.system(modelcmd)
+
+                    print('>>> Create base-folder on remote machine: {}'.format(mkdircmd))
+                    os.system(mkdircmd)
+
+                    print('>>> Sync content from local to remote machine: {}'.format(synccmd))
+                    os.system(synccmd)
+                    #input('...')
+
+                    print('>>> Execute pre-processing and classification on remote machine: {}'.format(sshcmd))
+                    os.system(sshcmd)
+                    #input('...')
+
+                    print('>>> Sync results back to local machine: {}'.format(resynccmd))
+                    os.system(resynccmd)
+                    #input('...')
+
+    end = timer()
+    print('\n(runtime : %.3f' % (end-start),'seconds)\n')
+
+    exit()
