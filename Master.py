a7e358603180ee1a7cc142dd3018dd1e1d5481e4
niklaas-0rbit <niklaas@0rb.it>
2021-02-20 14:45:00 +0100

    Update Master.py
    improved formatting

--- Master.py
+++ Master.py
@@ -8,6 +8,7 @@ Created on Sat Jan 17 13:53:04 2021
 
 import os
 import config as cfg
+
 from colorama import Fore, Style
 from timeit import default_timer as timer
 
@@ -30,19 +31,22 @@ if __name__ == '__main__':
     start = timer()
 
     verbose = args.verbose
+    local   = args.local
 
     # check passed optional arguments and commands
     print('\n'+40*'~'+' SCRIPT: Master.py '+40*'~')
     if verbose:
-        print('\nfiles:\n\t{}\n\nsampling-modes:\n\t{}\n\nfeature-vectors:\n\t{}\n'.format(cfg.filenames,cfg.modes,cfg.vectors))
+        print('\nfiles:\n\t{}\n\nsampling-modes:\n\t{}\n\nfeature-vectors:\n\t{}\n'.format(cfg.filenames,cfg.mode,cfg.vectors))
         print(20*'~'+' configuration '+20*'~')
         print('\n\tfile:\t{}\n\tvector:\t{}\n\tmode:\t{}\n\tstep:\t{}\n\n'.format(cfg.file,cfg.vector,cfg.mode,cfg.steps))
         input('...')
 
     # check online-status of remote machine
-    ping = os.system('ping -c 1 {} >/dev/null 2>&1'.format(cfg.remoteip)) # ping remote machine
-    if ping == 0: print('>>> Status {}: '.format(cfg.remoteip)+Fore.GREEN+'ONLINE'+Style.RESET_ALL)
-    else: print('>>> Status {}: '.format(cfg.remoteip)+Fore.RED+'OFFLINE'+Style.RESET_ALL), exit()
+    if not local:
+        ping = os.system('ping -c 1 {} >/dev/null 2>&1'.format(cfg.remoteip)) # ping remote machine
+
+        if ping == 0:   print('>>> Status {}: '.format(cfg.remoteip)+Fore.GREEN+'ONLINE'+Style.RESET_ALL)
+        else:           print('>>> Status {}: '.format(cfg.remoteip)+Fore.RED+'OFFLINE'+Style.RESET_ALL), exit()
 
     # iterate over configurations
     for file in cfg.file:
@@ -50,14 +54,15 @@ if __name__ == '__main__':
 
             # folder selection based on given feature-vector
             if vector < cfg.vectorlimit:
-                basefolder = cfg.flowfolder
-                sarg = '-f'
-                foldername = '{}_mode{}_vector{}_steps{}_perflowsampled'
+                basefolder  = cfg.flowfolder
+                sarg        = '-f'
+                foldername  = '{}_mode{}_vector{}_steps{}_perflowsampled'
             else:
-                basefolder = cfg.packetfolder
-                sarg = '-p'
-                foldername = '{}_mode{}_vector{}_steps{}_packetsampled'
-            mkdircmd = "ssh {} 'mkdir -p {}'".format(cfg.remote,basefolder)
+                basefolder  = cfg.packetfolder
+                sarg        = '-p'
+                foldername  = '{}_mode{}_vector{}_steps{}_packetsampled'
+
+            mkdir = "ssh {} 'mkdir -p {}'".format(cfg.remote,basefolder)
 
             for mode in cfg.mode: # iterate over given sampling-modes
 
@@ -68,37 +73,47 @@ if __name__ == '__main__':
                 for steps in cfg.steps: # iterate over given sampling-steps
 
                     # forge folders & commands based on configuration
-                    currentfolder = foldername.format(cfg.filenames[file],mode,vector,steps)
-                    samplingcmd = 'python3 rpi-Sampling.py -e {} {} {} {} {}'.format(sarg,mode,file,steps,vector)
-                    modelcmd = 'python3 rpi-Preprocessing.py -e -s {} {} {} {} {} {}'.format(sarg,mode,file,steps,vector,cfg.batchsize)
-                    synccmd = r'rsync -avz --progress {}/{}/ {}:{}/{}/'.format(basefolder,currentfolder,cfg.remote,basefolder,currentfolder)
-                    sshcmd = "ssh {} 'cd {} && python3 -u rpi-Preprocessing.py -e -r -m {} {} {} {} {} {}'".format(cfg.remote,cfg.remotewd,sarg,mode,file,steps,vector,cfg.batchsize) # importing model on remote
-                    #sshcmd = "ssh {} 'cd {} && python3 -u rpi-Preprocessing.py -e -r -s {} {} {} {} {} {}'".format(cfg.remote,cfg.remotewd,sarg,mode,file,steps,vector,cfg.batchsize) # saving model on remote
-                    resynccmd = r'rsync -avz --progress {}:{}/{}/ {}/{}/'.format(cfg.remote,basefolder,currentfolder,basefolder,currentfolder)
+                    folder   = foldername.format(cfg.filenames[file],mode,vector,steps)
+                    sampling = 'python3 rpi-Sampling.py -e {} {} {} {} {}'.format(sarg,mode,file,steps,vector)
+                    model    = 'python3 rpi-Preprocessing.py -e -s {} {} {} {} {} {}'.format(sarg,mode,file,steps,vector,cfg.batchsize)
+                    ssh      = "ssh {} 'cd {} && python3 -u rpi-Preprocessing.py -e -r -m {} {} {} {} {} {}'".format(cfg.remote,cfg.remotewd,sarg,mode,file,steps,vector,cfg.batchsize) # importing model on remote
+                    #ssh      = "ssh {} 'cd {} && python3 -u rpi-Preprocessing.py -e -r -s {} {} {} {} {} {}'".format(cfg.remote,cfg.remotewd,sarg,mode,file,steps,vector,cfg.batchsize) # saving model on remote
 
-                    # subsequential script & command exections
-                    print('>>> Sample PCAP on local machine: {}'.format(samplingcmd))
-                    os.system(samplingcmd)
+                    sync         = r'rsync -avz --progress {}/{}/ {}:{}/{}/'.format(basefolder,folder,cfg.remote,basefolder,folder)
+                    resync       = r'rsync -avz --progress {}:{}/{}/ {}/{}/'.format(cfg.remote,basefolder,folder,basefolder,folder)
 
-                    print('>>> Create and save model on local machine: {}'.format(modelcmd))
-                    os.system(modelcmd)
 
-                    print('>>> Create base-folder on remote machine: {}'.format(mkdircmd))
-                    os.system(mkdircmd)
+                    # LOCAL
+                    print('>>> Sample PCAP on local machine: {}'.format(sampling))
+                    os.system(sampling)
 
-                    print('>>> Sync content from local to remote machine: {}'.format(synccmd))
-                    os.system(synccmd)
+                    print('>>> Create and save model on local machine: {}'.format(model))
+                    os.system(model)
+
+                    if local:
+                        end = timer()
+                        print('\n(runtime : %.3f' % (end-start),'seconds)\n')
+                        continue
+
+
+                    # REMOTE
+                    print('>>> Create base-folder on remote machine: {}'.format(mkdir))
+                    os.system(mkdir)
+
+                    print('>>> Sync content from local to remote machine: {}'.format(sync))
+                    os.system(sync)
                     #input('...')
 
-                    print('>>> Execute pre-processing and classification on remote machine: {}'.format(sshcmd))
-                    os.system(sshcmd)
+                    print('>>> Execute pre-processing and classification on remote machine: {}'.format(ssh))
+                    os.system(ssh)
                     #input('...')
 
-                    print('>>> Sync results back to local machine: {}'.format(resynccmd))
-                    os.system(resynccmd)
+                    print('>>> Sync results back to local machine: {}'.format(resync))
+                    os.system(resync)
                     #input('...')
 
     end = timer()
+    print(20*'#')
     print('\n(runtime : %.3f' % (end-start),'seconds)\n')
 
     exit()
