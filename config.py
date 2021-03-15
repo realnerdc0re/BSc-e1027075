b53b2177e2ca64d6f007a611ccedc7636ac34d82
niklaas-0rbit <niklaas@0rb.it>
2021-03-15 16:59:54 +0100

    various

--- config.py
+++ config.py
@@ -118,13 +118,13 @@ remote      = '{}@{}'.format(remoteuser,remoteip)
 # EXPERIMENTS
 # batchsize for preprocessing
 # files, feature-vectors, sampling-modes & sampling-steps to process
-batchsize   = 100000
-file        = [0]
-vector      = [4,5]
-mode        = [1,5]
-steps       = [5,10]
+batchsize   = 100000 # batchsize used for Standard Scaler partial fit
+file        = [5]
+vector      = [4]
+mode        = [1]
+steps       = [5]
 n_PCA       = 4 # number of components for PCA
-chunksize   = 1 # read CSV line-by-line
+chunksize   = 10**5 # read CSV in chunks (reading line-by-line not allowed!)
 split       = 5000 # used for packetsampling, determines number of packets in editcap splits
 splitsize   = 25*10**4 # to not exceed rpi RAM size, split files into 250k rows per file
 #######################################################################################
