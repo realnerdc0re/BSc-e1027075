bd17e79f2c0bd511009e6dff6fbea4b30d854f0c
niklaas-0rbit <niklaas@0rb.it>
2021-07-13 15:36:11 +0200

    removed unused TCPflags dictionary

--- config.py
+++ config.py
@@ -103,18 +103,6 @@ modelimit       = 4 # used to determine choice for samplingmodes in evaluation
 flowlimit       = 4 # used to set mode to 'AGM' or '5tuple' for Labeling.py
 packetlimit     = 4
 #######################################################################################
-tcpflags = {
-        'A':100000000, # ACK
-        'P': 10000000, # PSH
-        'F':  1000000, # FIN
-        'R':   100000, # RST
-        'S':    10000, # SYN
-        'U':     1000, # URG
-        'E':      100, # ECE
-        'C':       10, # CWR
-        'N':        1  # NS
-    }
-#######################################################################################
 
 #######################################################################################
 # EXPERIMENT CONFIGURATION
@@ -133,10 +121,10 @@ remoteconf  = '/home/{}/{}/{}'.format(remoteuser,remotewd,configuration)
 # EXPERIMENTS
 # batchsize for preprocessing
 # files, feature-vectors, sampling-modes & sampling-steps to process
-file        = [0]
-vector      = [6]
-mode        = [7]
-steps       = [7] # 0 to process unsampled PCAP
+file        = [3]
+vector      = [5]
+mode        = [5]
+steps       = [0] # 0 to process unsampled PCAP
 seed        = 1000 # seed number used for random sampling
 
 n_PCA       = 12 # unused, number is stored in information.csv
