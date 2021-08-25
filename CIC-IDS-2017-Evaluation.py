e8281e987068dde4e588392d296ecbe037dbbee0
niklaas-0rbit <niklaas@0rb.it>
2021-08-25 17:26:17 +0200

    attack distribution
    added possibility to create histogram for attack distributions

--- CIC-IDS-2017-Evaluation.py
+++ CIC-IDS-2017-Evaluation.py
@@ -7,7 +7,7 @@ import os
 
 from pandas import read_csv
 from pandas.plotting import scatter_matrix
-from matplotlib import pyplot
+import matplotlib.pyplot as plt
 from scipy.stats import zscore
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import MinMaxScaler
@@ -79,20 +79,77 @@ if __name__ == '__main__':
 
     verbose = True
     path = mntd / 'data' / 'CIC-IDS2017' / 'PCAP' / 'Original PCAPs'
-    files = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Merged']
+    files = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday']
+    #files = ['Merged']
     filename = '{}_flows.csv'
 
     # COMMAND TO CREATE FLOWS
     #~/Git/go-flows/./go-flows run features ~/Git/BSc-e1027075/go-flows-configurations/CAIA_packetSampling.json export csv Merged_flows_unlabeled.csv source libpcap Merged.pcap
-
+    #~/Git/go-flows/./go-flows run features ~/Git/BSc-e1027075/go-flows-configurations/AGM_10s.json export csv Merged_flows_unlabeled.csv source libpcap Merged.pcap
     # COMMAND TO LABEL FLOWS
     #python3 /mnt/data/BSc-e1027075/Labeling.py /mnt/data/CIC-IDS2017/PCAP/Original\ PCAPs/Merged_flows 5tuple
+    #python3 /mnt/data/BSc-e1027075/Labeling.py /mnt/data/CIC-IDS2017/PCAP/Original\ PCAPs/Merged_flows AGM
+
 
     for file in files:
         currentfile = filename.format(file)
         filepath = path / currentfile
         dataset = importCSV(filepath,None,False,None) # import current file
         print('\n',dataset.groupby('Label').size()) # outputs distribution of attack and benign labeled flows
+        #print('\n',dataset.groupby('Attack').size()) # outputs distribution of attack and benign labeled flows
+
+        attackdistribution = dataset.groupby('Attack').size() # creates distribution of all attack types to create histogram
+        print('\n{}\n{}'.format(attackdistribution, type(attackdistribution)))
         input('...')
+        ad = attackdistribution # used for histogram generation
 
     exit()
+
+    if false: # following code block should be placed within the for-loop to generate histogram (at the end not used in the thesis, instead created a table)
+        attacks = []
+        numbers = []
+
+        n = len(ad)
+
+        for i in range(0,n):
+            attacks.append(ad.index[i])
+            numbers.append(ad[i])
+
+        attacks.pop(10)
+        numbers.pop(10)
+
+        attacks[9] = 'Infiltration:Dropbox download' # rename to increase readability (bad one because with AGM there is actually exactly that name used for other attack type)
+
+        tickx = [0,0.25,0.5,0.75,1,1.25,1.5,1.75,2,2.25,2.5,2.75,3,3.25]
+
+
+        print('{}\n{}'.format(attacks,numbers))
+
+        #fig = plt.figure(figsize=(21.0,9.0),frameon=True)
+        fig,ax = plt.subplots(figsize=(21.0,9.0))
+        ax.spines["right"].set_visible(False)
+        ax.spines["left"].set_visible(False)
+        ax.spines["top"].set_visible(False)
+
+        #plt.title('CAIA',ha='center',fontsize=16) # set title
+        plt.bar(tickx,numbers,color='#6E6E74',width=0.125,edgecolor='#4F4F5A',linewidth=2,tick_label=attacks)
+        plt.xticks(rotation=80,size=14)
+        plt.yticks([]) # dont plot any numbers on the y-axiy
+        #plt.tick_params(top=False,bottom=False)
+
+        #plt.xlabel('Attacks')
+        #plt.ylabel('Numbers')
+        move = [-0.035,-0.035,-0.05,-0.065,-0.05,-0.075,-0.05,-0.05,-0.015,-0.065,-0.08,-0.035,-0.03,-0.035] # manually adjust positioning CAIA
+        for i,v in enumerate(numbers):
+            xvalue = tickx[i] + move[i]
+            plt.text(x=xvalue,y=v+2500,s=v,size=14)
+
+        plt.subplots_adjust(bottom=0.35) # create space for attack labels
+        #plt.axis('off')
+
+        plt.savefig('/home/noooberino/Git/BSc-e1027075-Thesis/graphics/figures/CAIAdistribution.pdf',bbox_inches='tight',pad_inches=0) # save plot to file
+        #plt.imsave('/home/noooberino/Git/BSc-e1027075-Thesis/graphics/figures/CAIAdistribution.pdf')
+        plt.show()
+
+        #ad.plot.bar()
+        #pyplot.hist(attackdistribution.)
